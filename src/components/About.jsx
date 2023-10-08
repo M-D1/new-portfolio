f64b0dbf110e9655c8e79966a58249d00e28ad74
Majd <maddm205@gmail.com>
2023-10-08 14:52:39 +0300

    ''

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,10 +10,11 @@ function About(){
                 <div className='sm:flex items-center max-w-5xl mx-auto'>
                     <p className='px-8 text-sm mt-5 sm:text-base tracking-wider sm:w-1/2 lg:text-2xl lg:leading-10 '>
                         Hi I am Majd Ali frontend developer .
-                        I am Open to job apportonuties work with a company or freelance work 
+                         
                         Nice to meet you !
                         I have expriance in Html, CSS, JavaScript, React.js, tailwind css.
                         Always looking to learn new technologies and improve my self to be better than before.
+                        I am Open to job apportonuties work with a company or freelance work
 
                     </p>
                     <img src={about} alt="" className='saturate-[.2] mx-auto w-4/5 h-4/5  sm:w-1/2 sm:h-1/2'/>
